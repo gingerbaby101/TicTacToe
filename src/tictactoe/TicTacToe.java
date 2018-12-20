9e025183b597a007d58a8d4bc39f3edcba60bcba
Ian Howell <ianh413@gmail.com>
2018-12-20 00:01:47 -0600

    Possibly working minimax.

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -12,7 +12,11 @@ import java.util.Scanner;
  * @author ACER
  */
 public class TicTacToe {
-    public static int status(int[][] board) {
+    Board currentBoard;
+
+    TicTacToe() {}
+
+    private int status(int[][] board) {
         if (board[0][0] == board[0][1] && board[0][0] == board[0][2] && board[0][0] != 0)
             return board[0][0];
         if (board[1][0] == board[1][1] && board[1][0] == board[1][2] && board[1][0] != 0)
@@ -46,7 +50,7 @@ public class TicTacToe {
      * @param maxPlayer
      * @return
      */
-    public static int miniMax(Board board, int depth, boolean maxPlayer) {
+    private int miniMax(Board board, int depth, boolean maxPlayer) {
         if(board.status() == 1) {
             return 1;
         } else if(board.status() == -1) {
@@ -81,106 +85,126 @@ public class TicTacToe {
         return -666;
     }
 
-    public static int iOfBest(ArrayList<Integer> list) {
-        // TODO: Change this to just search and find a max. Much
-        // better complexity as sorting takes O(n log(n)) and finding
-        // a max only takes O(n)
-        ArrayList<Integer> copy =  new ArrayList<>(list);
-        Collections.sort(copy);
-        //System.out.println("copy: " + copy);
-        int best = copy.get(copy.size()-1);
-        //    for(int i = 0; i < copy.size() - 1; i++)
-        //    {
-        //        best = Math.max(copy.get(i), copy.get(i + 1));
-        //    }
-        return list.indexOf(best);
+    private int iOfBest(ArrayList<Integer> list) {
+        if (list.size() == 0)
+            return -1;
+        int maxIndex = 0;
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) > list.get(maxIndex))
+                maxIndex = i;
+        }
+        return maxIndex;
     }
 
-    public static int[][] realClone(int[][] k) {
-        int[][] res = new int[k.length][k[0].length];
-        for(int r = 0; r < k.length; r++) {
-            for(int c = 0; c < k[0].length; c++) {
-                res[r][c] = k[r][c];
+    public void getUserTurn() {
+        //Prints the game
+        currentBoard.print();
+
+        System.out.println("Row:");
+        Scanner poop = new Scanner(System.in);
+        int row = poop.nextInt() - 1;
+
+        System.out.println("Collumn:");
+        poop = new Scanner(System.in);
+        int col = poop.nextInt() - 1;
+
+        currentBoard = currentBoard.setSlot(row, col, -1);
+    }
+
+    public void getComputerTurn() {
+        //System.out.println("cellsLeft of currentBoard: " + cellsLeft(currentBoard));
+        Board temp = new Board(currentBoard);
+        System.out.println("after constructor: " + currentBoard.getCells());
+        //System.out.println("cellsLeft of temp: " + cellsLeft(temp));
+        ArrayList<Integer> movesTrue = new ArrayList<>();
+        ArrayList<Integer> movesFalse = new ArrayList<>();
+        //boolean leaveLoop = false;
+
+        int maxValue = -666;
+        Pair<Integer, Integer> best_coordinates = new Pair<Integer, Integer>();
+        boolean valid_coordinates = false;
+
+        for(int r = 0; r < currentBoard.getRLength(); r++) {
+            for(int c = 0; c < currentBoard.getCLength(); c++) {
+                if(temp.getSlot(r, c) == 0) {
+                    int value = miniMax(currentBoard.setSlot(r, c, 1), currentBoard.getCells() - 1, true);
+                    if (value > maxValue) {
+                        valid_coordinates = true;
+                        best_coordinates.first = r;
+                        best_coordinates.second = c;
+                    }
+                }
             }
         }
-        return res;
+
+        if (valid_coordinates) {
+            currentBoard = currentBoard.setSlot(best_coordinates.first, best_coordinates.second, 1);
+        } else {
+            System.out.println("ERROR: No best move found, no moves available.");
+        }
+    }
+
+    public boolean checkEndOfGame() {
+
+        if (currentBoard.status() == 1) {
+            System.out.println("One wins");
+            return true;
+        } else if (currentBoard.status() == -1) {
+            System.out.println("Negative one wins");
+            return true;
+        } else if (currentBoard.getCells() == 0) {
+            System.out.println("Game resulted in tie");
+            return true;
+        }
+        return false;
+    }
+
+
+    public void play() {
+        currentBoard = new Board();
+        int counter = 0;
+        //System.out.println("initial cellsLeft: " + cellsLeft(currentBoard));
+        boolean keepPlaying = true;
+        while(keepPlaying) {
+            getUserTurn();
+            keepPlaying = !checkEndOfGame();
+            // System.out.println("after changeTo: " + currentBoard.getCells());
+
+
+            getComputerTurn();
+            keepPlaying = !checkEndOfGame();
+
+            // System.out.println("If maximizing player = true: " + movesTrue);
+            // System.out.println("If maximizing player = false: " + movesFalse);
+            // //int iBest = iOfBest(movesTrue);
+            // for(int r = 0; r < currentBoard.getRLength(); r++) {
+            //     for(int c = 0; c < currentBoard.getCLength(); c++) {
+            //         //System.out.println(temp[r][c] == 0);
+            //         //System.out.println("best move at index: " + iOfBest(moves) + "  counter:" + counter);
+
+            //         //                if(temp[r][c] != 0 && iBest == counter)
+
+            //         if(temp.getSlot(r, c) == 0 && iOfBest(movesTrue) == counter) {
+            //             currentBoard = currentBoard.setSlot(r, c, 1);
+            //             //System.out.println("Played at " + r + "  " + c);
+            //             counter = 1000;
+            //         }
+            //         if(temp.getSlot(r, c) == 0)
+            //             counter++;
+            //     }
+            // }
+
+            // counter = 0;
+            // if (currentBoard.status() == 1)
+            //     System.out.println("One wins");
+            // if(currentBoard.status() == -1)
+            //     System.out.println("Negative one wins");
+        }
+        currentBoard.print();
     }
 
     public static void main(String[] args) {
-        Board example = new Board();
-        int counter = 0;
-        //System.out.println("initial cellsLeft: " + cellsLeft(example));
-        while(example.getCells() != 0) {
-            //Prints the game
-            example.print();
-
-            System.out.println("Row:");
-            Scanner poop = new Scanner(System.in);
-            int row = poop.nextInt() - 1;
-
-            System.out.println("Collumn:");
-            poop = new Scanner(System.in);
-            int col = poop.nextInt() - 1;
-
-            example.changeTo(example.setSlot(row, col, -1));
-            System.out.println("after changeTo: " + example.getCells());
-            if (example.status() == 1) {
-                System.out.println("One wins");
-                break;
-            } else if (example.status() == -1) {
-                System.out.println("Negative one wins");
-                break;
-            }
-            //System.out.println("cellsLeft of example: " + cellsLeft(example));
-            Board temp = new Board(example);
-            System.out.println("after constructor: " + example.getCells());
-            //System.out.println("cellsLeft of temp: " + cellsLeft(temp));
-            ArrayList<Integer> movesTrue = new ArrayList<>();
-            ArrayList<Integer> movesFalse = new ArrayList<>();
-            //boolean leaveLoop = false;
-            for(int r = 0; r < example.getRLength(); r++) {
-                for(int c = 0; c < example.getCLength(); c++) {
-                    if(temp.getSlot(r, c) == 0) {
-                        temp.changeTo(example.setSlot(row, col, 1));
-                        int left = temp.getCells();
-                        System.out.println("cells left: " + temp.getCells());
-                        int valTrue = miniMax(temp, left, true);
-                        int valFalse = miniMax(temp, left, false);
-                        //System.out.println("AFTER miniMax " + cellsLeft(temp));
-                        movesTrue.add(valTrue);
-                        movesFalse.add(valFalse);
-
-                        temp = new Board(example);
-                    }
-                }
-            }
-
-            System.out.println("If maximizing player = true: " + movesTrue);
-            System.out.println("If maximizing player = false: " + movesFalse);
-            //int iBest = iOfBest(movesTrue);
-            for(int r = 0; r < example.getRLength(); r++) {
-                for(int c = 0; c < example.getCLength(); c++) {
-                    //System.out.println(temp[r][c] == 0);
-                    //System.out.println("best move at index: " + iOfBest(moves) + "  counter:" + counter);
-
-                    //                if(temp[r][c] != 0 && iBest == counter)
-
-                    if(temp.getSlot(r, c) == 0 && iOfBest(movesTrue) == counter) {
-                        example.changeTo(example.setSlot(r, c, 1));
-                        //System.out.println("Played at " + r + "  " + c);
-                        counter = 1000;
-                    }
-                    if(temp.getSlot(r, c) == 0)
-                        counter++;
-                }
-            }
-
-            counter = 0;
-            if (example.status() == 1)
-                System.out.println("One wins");
-            if(example.status() == -1)
-                System.out.println("Negative one wins");
-        }
-        example.print();
+        TicTacToe game = new TicTacToe();
+        game.play();
     }
 }
